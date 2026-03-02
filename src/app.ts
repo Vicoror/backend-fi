bd6af7d255d3d3a4e43b94d726ae20467d24fc17
victoriaorozco <vicoror@gmail.com>
2026-03-02 14:22:46 -0600

    carr

--- src/app.ts
+++ src/app.ts
@@ -48,7 +48,8 @@ app.use('/webhook', express.raw({ type: 'application/json' }), webhookRoutes);
 // =============================================
 // 3. TERCERO: MIDDLEWARES NORMALES
 // =============================================
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(cookieParser());
 
 // =============================================
